d0c1f8d8ca1cf998918a964dc0f097d48baddf34
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-07-10 00:55:37 +0300

    some more comments

--- src/main/java/com/mytests/spring/jpa/reactiverepositories/repositories/UserReactiveRepo.java
+++ src/main/java/com/mytests/spring/jpa/reactiverepositories/repositories/UserReactiveRepo.java
@@ -16,27 +16,32 @@ import java.math.BigInteger;
  */
 public interface UserReactiveRepo extends ReactiveCrudRepository<User, BigInteger> {
 
-   //Flux<User> findByAgeIsNotIn(Mono<Integer> age);  // incorrect, but no errors
+    // https://youtrack.jetbrains.com/issue/IDEA-272981
+    //Flux<User> findByAgeIsNotIn(Mono<Integer> age);  // incorrect, but no errors
     Flux<User> findByAgeIsNotIn(Flux<Integer> age);
+
     Flux<User> findByAgeBetween(Mono<Integer> age, Mono<Integer> age2);  //  ok
-   //Flux<User> findByAgeBetween(Flux<Integer> age); // error shown: expected int, int   - ok
-  
-   // Flux<User> findByAge(Flux<Integer> age);  // incorrect, but no errors are shown
+    //Flux<User> findByAgeBetween(Flux<Integer> age); // error shown: expected int, int   - ok
+
+    // Flux<User> findByAge(Flux<Integer> age);  // incorrect, but no errors are shown
     Flux<User> findByAge(Mono<Integer> age);   // ok
-    
-    Flux<User> findByNameMatchesRegex(String name);  
-    
+
+    Flux<User> findByNameMatchesRegex(String name);
+
     Flux<User> findByNameAndAge(String name, Mono<Integer> age);
+
     Mono<Boolean> existsByAge(int age);     // error is shown  https://youtrack.jetbrains.com/issue/IDEA-260454
+
     Flux<String> findNamesByAgeLessThan(Mono<Integer> age); // error is shown
- 
-   //Mono<String> findNameByAgeGreaterThan(int age);   // no error is shown but should!
+
+    //Mono<String> findNameByAgeGreaterThan(int age);   // no error is shown but should!
     Mono<String> findFirstNameByAgeGreaterThan(int age);
+
     Mono<Integer> countByNameContaining(Mono<String> name);
-    
-    
-     @Query("{ 'name' : ?0 }")
+
+
+    @Query("{ 'name' : ?0 }")
     Flux<User> findByName(String name);
-    
-    
+
+
 }
